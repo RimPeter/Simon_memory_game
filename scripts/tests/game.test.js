6638a32e302a2dc666c0a728cb73b404dca30fbf
RimPeter <hull@localappliancerentals.co.uk>
2024-04-09 15:37:35 +0100

    Run tests
      game object contains correct keys
        √ score key exists (1 ms)
        √ currentGame key exists (1 ms)
        √ playerMoves key exists (1 ms)
        √ choices key exists (1 ms)
        √ choices key contains the correct values (1 ms)
      newGame works correctly
        √ score is 0
        √ Clear the playerMoves array
        √ Clear the currentGame array
        √ should display 0 in the #score element

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,3 +2,54 @@
  * @jest-environment jsdom
  */
 
+// Import the functions you want to test
+const { game, newGame, showScore } = require("../game");
+const { beforeAll, test } = require("@jest/globals");
+
+beforeAll(() => {
+  let fs = require("fs");
+  let fileContents = fs.readFileSync("index.html", "UTF-8");
+  document.open();
+  document.write(fileContents);
+  document.close();
+});
+
+describe("game object contains correct keys", () => {
+  test("score key exists", () => {
+    expect("score" in game).toBe(true);
+  });
+  test("currentGame key exists", () => {
+    expect("currentGame" in game).toBe(true);
+  });
+  test("playerMoves key exists", () => {
+    expect("playerMoves" in game).toBe(true);
+  });
+  test("choices key exists", () => {
+    expect("choices" in game).toBe(true);
+  });
+  test("choices key contains the correct values", () => {
+    expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+  });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.currentGame = ["button1", "button2"];
+        game.playerMoves = ["button1", "button2"];
+        document.getElementById("score").innerText = 42;
+        newGame();
+    });
+    test("score is 0", () => {
+        expect(game.score).toBe(0);
+    });
+    test("Clear the playerMoves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("Clear the currentGame array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should display 0 in the #score element", () => {
+        expect(document.getElementById("score").innerText).toBe(0);
+    });
+});
